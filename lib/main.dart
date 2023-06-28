70e8bdfa22012b1e501ee4345924b643ba2fbbd5
Matteo <teotechplus@gmail.com>
2023-06-28 16:13:58 +0200

    Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,252 +1,200 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:porcupine_flutter/porcupine.dart';
-import 'package:porcupine_flutter/porcupine_manager.dart';
-import 'package:porcupine_flutter/porcupine_error.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainWidget(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  final String accessKey =
-      "BIEPMllOI32xRmMX+nphrmhKeKBRq0OXvzULJTqCRLT2Of9IXDkt7g=="; // AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)
-
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  late List<String> _keywords;
-
-  bool isError = false;
-  String errorMessage = "";
-
-  bool isButtonDisabled = false;
-  bool isProcessing = false;
-  Color detectionColour = Color(0xff00e5c3);
-  Color defaultColour = Color(0xfff5fcff);
-  Color? backgroundColour;
-  String currentKeyword = "StopIt";
-  PorcupineManager? _porcupineManager;
-
+class MainWidget extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    setState(() {
-      isButtonDisabled = true;
-      backgroundColour = defaultColour;
-    });
-    WidgetsBinding.instance.addObserver(this);
+  _MainWidgetState createState() => _MainWidgetState();
+}
 
-    loadNewKeyword(currentKeyword);
+class _MainWidgetState extends State<MainWidget> {
+  List<Workout> workouts = [];
+
+  void addWorkout() {
+    setState(() {
+      workouts.add(Workout(
+        key: UniqueKey(),
+        index: workouts.length, // Assign the index of the workout
+        removeWorkout: removeWorkout,
+      ));
+    });
   }
 
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
+  void removeWorkout(Workout workout) {
+    setState(() {
+      workouts.remove(workout);
+    });
+  }
+
+  void onReorder(int oldIndex, int newIndex) {
+    setState(() {
+      if (newIndex > oldIndex) {
+        newIndex -= 1;
+      }
+      final workout = workouts.removeAt(oldIndex);
+      workouts.insert(newIndex, workout);
+    });
   }
 
   @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    if (state == AppLifecycleState.paused) {
-      await _stopProcessing();
-      await _porcupineManager?.delete();
-      _porcupineManager = null;
-    }
-  }
-
-// Load new keyword
-  Future<void> loadNewKeyword(String keyword) async {
-    setState(() {
-      isButtonDisabled = true;
-    });
-
-    if (isProcessing) {
-      await _stopProcessing();
-    }
-
-    if (_porcupineManager != null) {
-      await _porcupineManager?.delete();
-      _porcupineManager = null;
-    }
-    try {
-      var platform = (Platform.isAndroid) ? "android" : "ios";
-      var keywordPath = "assets/keywords/$platform/${keyword}_$platform.ppn";
-
-      _porcupineManager = await PorcupineManager.fromKeywordPaths(
-          accessKey, [keywordPath], wakeWordCallback);
-
-      setState(() {
-        isError = false;
-      });
-    }
-    // Catch errors
-    on PorcupineInvalidArgumentException catch (ex) {
-      errorCallback(PorcupineInvalidArgumentException(
-          "${ex.message}\nEnsure your accessKey '$accessKey' is a valid access key."));
-    } on PorcupineActivationException {
-      errorCallback(
-          PorcupineActivationException("AccessKey activation error."));
-    } on PorcupineActivationLimitException {
-      errorCallback(PorcupineActivationLimitException(
-          "AccessKey reached its device limit."));
-    } on PorcupineActivationRefusedException {
-      errorCallback(PorcupineActivationRefusedException("AccessKey refused."));
-    } on PorcupineActivationThrottledException {
-      errorCallback(PorcupineActivationThrottledException(
-          "AccessKey has been throttled."));
-    } on PorcupineException catch (ex) {
-      errorCallback(ex);
-    } finally {
-      setState(() {
-        isButtonDisabled = false;
-      });
-    }
-  }
-
-// Function called when an error occurs
-  void errorCallback(PorcupineException error) {
-    setState(() {
-      isError = true;
-      errorMessage = error.message!;
-    });
-  }
-
-// Function called when the start button is pressed
-  Future<void> _startProcessing() async {
-    setState(() {
-      isButtonDisabled = true;
-    });
-
-    if (_porcupineManager == null) {
-      await loadNewKeyword(currentKeyword);
-    }
-
-    try {
-      await _porcupineManager?.start();
-      setState(() {
-        isProcessing = true;
-      });
-    } on PorcupineException catch (ex) {
-      errorCallback(ex);
-    } finally {
-      setState(() {
-        isButtonDisabled = false;
-      });
-    }
-  }
-
-// Stop processing
-  Future<void> _stopProcessing() async {
-    setState(() {
-      isButtonDisabled = true;
-    });
-
-    await _porcupineManager?.stop();
-
-    setState(() {
-      isButtonDisabled = false;
-      isProcessing = false;
-    });
-  }
-
-// Build start button
-  buildStartButton(BuildContext context) {
-    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-      primary: picoBlue,
-      shape: CircleBorder(),
-      textStyle: TextStyle(color: Colors.white),
-    );
-
-    return Expanded(
-      flex: 2,
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
       child: Container(
-        child: SizedBox(
-          width: 150,
-          height: 150,
-          child: ElevatedButton(
-            style: buttonStyle,
-            onPressed: isProcessing ? _stopProcessing : _startProcessing,
-            child: Text(
-              isProcessing ? "Stop" : "Start",
-              style: TextStyle(fontSize: 30),
+        padding: EdgeInsets.all(16.0),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xff252525),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height:
+                  100, // Adjust the height as needed for the logo and settings wheel
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Add your logo widget here
+                  Icon(
+                    Icons.ac_unit, // Replace with your logo icon
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                  // Add your settings wheel widget here
+                  IconButton(
+                    icon: Icon(
+                      Icons.settings,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      // Handle settings button press
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
+            SizedBox(
+                height: 16.0), // Add spacing between the header and the list
+            Expanded(
+              child: ReorderableListView(
+                onReorder: onReorder,
+                children: workouts.map((workout) {
+                  return workout;
+                }).toList(),
+              ),
+            ),
+            SizedBox(
+                height: 16.0), // Add spacing between the list and the button
+            SizedBox(
+              width: double.infinity, // Make the button occupy the entire width
+              child: ElevatedButton(
+                onPressed: addWorkout,
+                child: Text(
+                  'Add Workout',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[850],
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange[900],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+}
 
-// Build error message
-  buildErrorMessage(BuildContext context) {
-    return Expanded(
-        flex: 1,
-        child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(left: 20, right: 20),
-            decoration: !isError
-                ? null
-                : BoxDecoration(
-                    color: Colors.red, borderRadius: BorderRadius.circular(5)),
-            child: !isError
-                ? null
-                : Text(
-                    errorMessage,
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  )));
+class Workout extends StatefulWidget {
+  Workout({Key? key, required this.index, required this.removeWorkout})
+      : super(key: key);
+
+  final int index;
+  final Function(Workout) removeWorkout;
+
+  @override
+  _WorkoutState createState() => _WorkoutState();
+}
+
+class _WorkoutState extends State<Workout> {
+  bool isExpanded = false;
+
+  void toggleExpansion() {
+    setState(() {
+      isExpanded = !isExpanded;
+    });
   }
 
-  String detectedWord = "";
-// Function called when wake word is detected
-  void wakeWordCallback(int keywordIndex) {
-    if (keywordIndex >= 0) {
-      setState(() {
-        backgroundColour = detectionColour;
-        detectedWord = "Mannaccia a te";
-      });
-      Future.delayed(const Duration(milliseconds: 1000), () {
-        setState(() {
-          backgroundColour = defaultColour;
-          detectedWord = "";
-        });
-      });
-    }
+  void removeWorkout() {
+    widget.removeWorkout(this.widget);
   }
-
-// Build page
-  Color picoBlue = Color.fromRGBO(55, 125, 255, 1);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        key: _scaffoldKey,
-        backgroundColor: backgroundColour,
-        appBar: AppBar(
-          title: const Text('Porcupine Demo'),
-          backgroundColor: picoBlue,
-        ),
-        body: Column(
-          children: [
-            buildStartButton(context),
-            buildErrorMessage(context),
-            SizedBox(
-                height:
-                    20), // Add a spacing between the existing widgets and the detected word
-            Text(
-              detectedWord, // Display the detected word
-              style: TextStyle(fontSize: 24),
+    return Container(
+      key: widget.key,
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.orange[800],
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: toggleExpansion,
+            child: Row(
+              children: [
+                Icon(
+                  isExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                ),
+                Text('Workout ${widget.index}'),
+              ],
             ),
-          ],
-        ),
+          ),
+          if (isExpanded)
+            Container(
+              margin: EdgeInsets.only(top: 8.0),
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text('Workout Content'),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.remove_circle),
+                    onPressed: removeWorkout,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+        ],
       ),
     );
   }
